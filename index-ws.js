10bb01bef7ae0448d5524e345d2a3a80bda23a53
himansri21 <himansrivastava2003@gmailcom>
2024-06-29 11:27:42 +0530

    changed index-ws,js

--- index-ws.js
+++ index-ws.js
@@ -1,41 +1,95 @@
 const express = require("express");
-const app = express(); // Initialize the Express app
-const server = require("http").createServer(); // Create the HTTP server and pass the Express app
+const server = require("http").createServer();
+const app = express();
+const PORT = 3000;
 
 app.get("/", function(req, res) {
   res.sendFile("index.html", { root: __dirname });
 });
 
 server.on("request", app);
-server.listen(4000, function() {
-  console.log("server started on port 3000");
+
+server.listen(PORT, function() {
+  console.log("Listening on " + PORT);
 });
 
+/** Websocket **/
 const WebSocketServer = require("ws").Server;
 
 const wss = new WebSocketServer({ server: server });
 
 wss.on("connection", function connection(ws) {
   const numClients = wss.clients.size;
-  console.log("Clients connected", numClients);
 
-  wss.broadcast(`current visitors : ${numClients}`);
+  console.log("clients connected: ", numClients);
+
+  wss.broadcast(`Current visitors: ${numClients}`);
 
   if (ws.readyState === ws.OPEN) {
-    ws.send(`welcome to my sever`);
+    ws.send("welcome!");
   }
 
-  ws.on(`close`, function close() {
-    ws.broadcast(`current visitors : ${numClients}`);
-    console.log(`A client has connected`);
+  ws.on("close", function close() {
+    wss.broadcast(`Current visitors: ${wss.clients.size}`);
+    console.log("A client has disconnected");
+  });
+
+  ws.on("error", function error() {
+    //
   });
 });
 
+/**
+ * Broadcast data to all connected clients
+ * @param  {Object} data
+ * @void
+ */
 wss.broadcast = function broadcast(data) {
+  console.log("Broadcasting: ", data);
   wss.clients.forEach(function each(client) {
     client.send(data);
   });
 };
+
+/** End Websocket **/
+// const express = require("express");
+// const app = express(); // Initialize the Express app
+// const server = require("http").createServer(); // Create the HTTP server and pass the Express app
+
+// app.get("/", function(req, res) {
+//   res.sendFile("index.html", { root: __dirname });
+// });
+
+// server.on("request", app);
+// server.listen(4000, function() {
+//   console.log("server started on port 3000");
+// });
+
+// const WebSocketServer = require("ws").Server;
+
+// const wss = new WebSocketServer({ server: server });
+
+// wss.on("connection", function connection(ws) {
+//   const numClients = wss.clients.size;
+//   console.log("Clients connected", numClients);
+
+//   wss.broadcast(`current visitors : ${numClients}`);
+
+//   if (ws.readyState === ws.OPEN) {
+//     ws.send(`welcome to my sever`);
+//   }
+
+//   ws.on(`close`, function close() {
+//     ws.broadcast(`current visitors : ${numClients}`);
+//     console.log(`A client has connected`);
+//   });
+// });
+
+// wss.broadcast = function broadcast(data) {
+//   wss.clients.forEach(function each(client) {
+//     client.send(data);
+//   });
+// };
 
 // const express = require("express");
 // const app = express(); // Initialize the Express app
