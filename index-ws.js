a92996b9dfd1b32bcd1c771f3f5dc3c73d90ddf5
himansri21 <himansrivastava2003@gmailcom>
2024-06-29 11:36:12 +0530

    using the process.env.Port to set port correctly

--- index-ws.js
+++ index-ws.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const server = require("http").createServer();
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", function(req, res) {
   res.sendFile("index.html", { root: __dirname });
